5707861def1659e27a28b241181965c59bb1f877
Mehedi Zaman <mehedi.zaman.professional@gmail.com>
2025-11-17 14:26:04 +0600

    Problem 5

--- solution.ts
+++ solution.ts
@@ -45,3 +45,15 @@ function filterByRating(items: Item[]): Item[] {
     const output: Item[] = items.filter(item => item.rating >= 4);
     return output;
 }
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+};
+
+function filterActiveUsers(users: User[]): User[] {
+    const output: User[] = users.filter(user => user.isActive);
+    return output;
+}
